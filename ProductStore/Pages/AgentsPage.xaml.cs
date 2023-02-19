acb968f003decef1c5599e4bc81962821e0ccd3f
Diyar Tesfaye <mr.vasya10.10@gmail.com>
2023-02-19 22:28:27 +0300

    Add project files.

---
+++ ProductStore/Pages/AgentsPage.xaml.cs
@@ -0,0 +1,215 @@
+﻿using ProductStore.DBContext;
+using ProductStore.Pages.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProductStore.Pages
+{
+    /// <summary>
+    /// Interaction logic for AgentsPage.xaml
+    /// </summary>
+    public partial class AgentsPage : Page
+    {
+        private List<AgentStat> _agentStats = new List<AgentStat>();
+        private List<AgentStat> _sortedStats = new List<AgentStat>();
+
+        private Dictionary<string, List<ProductSale>> _salesDict = new Dictionary<string, List<ProductSale>>();
+        private Dictionary<string, List<ProductSale>> _sortedSalesDict = new Dictionary<string, List<ProductSale>>();
+
+        public AgentsPage()
+        {
+            InitializeComponent();
+
+            var filterByList = MainWindow.db.AgentType.ToList();
+            filterByList.Add(new AgentType { ID = 0, Title = "Все типы" });
+            FilterByDateCb.ItemsSource = filterByList;
+            FilterByDateCb.DisplayMemberPath = "Title";
+            FilterByDateCb.SelectedItem = filterByList.Where(i => i.ID == 0).FirstOrDefault();
+
+            SortByDateCb.ItemsSource = new List<string>
+            {
+                "По умолчанию",
+                "Наименование (от а до я)",
+                "Наименование (от я до а)",
+                "Размер скидки (по возрастанию)",
+                "Размер скидки (по убыванию)"
+            };
+
+            SortByDateCb.SelectedItem = "По умолчанию";
+
+            List<ProductSale> productsales = MainWindow.db.ProductSale.ToList();
+
+            foreach(var item in productsales)
+            {
+                string key = $"{item.Agent.Title}|{item.Agent.Phone}|{item.Agent.AgentType.Title}|{item.AgentID}|/Images/{item.Agent.Logo}";
+                if(_salesDict.ContainsKey(key))
+                {
+                    _salesDict.TryGetValue(key, out var list);
+                    list.Add(item);
+                }
+                else
+                {
+                    var list = new List<ProductSale>
+                    {
+                        item
+                    };
+                    _salesDict.Add(key, list);
+                }
+            }
+
+            UpdateList();
+        }
+
+         private void UpdateList()
+        {
+            _sortedSalesDict.Clear();
+            _agentStats.Clear();
+
+            foreach(var item in _salesDict)
+            {
+                var list = item.Value.Where(i => i.SaleDate.Year == DateTime.Now.Year).ToList();
+                _sortedSalesDict.Add(item.Key, list);
+            }
+
+            foreach(var item in _sortedSalesDict)
+            {
+                string[] values = item.Key.Split('|');
+
+                int sales = 0;
+
+                foreach(var sale in item.Value)
+                {
+                    sales += sale.ProductCount;
+                }
+
+                double discount = 0;
+
+                if (sales > 10000 && sales < 50000)
+                    discount = 0.05;
+                if (sales > 50000 && sales < 150000)
+                    discount = 0.1;
+                if (sales > 150000 && sales < 500000)
+                    discount = 0.2;
+                if (sales > 500000)
+                    discount = 0.25;
+
+                _agentStats.Add(new AgentStat(values[0], sales, discount, values[1], values[2], values[4], Convert.ToInt32(values[3])));
+            }
+
+            switch(SortByDateCb.SelectedItem)
+            {
+                case "Наименование (от а до я)":
+                    _sortedStats = _agentStats.OrderBy(p => p.AgentName).ToList();
+                    break;
+                case "Наименование (от я до а)":
+                    _sortedStats = _agentStats.OrderByDescending(p => p.AgentName).ToList();
+                    break;
+                case "Размер скидки (по возрастанию)":
+                    _sortedStats = _agentStats.OrderBy(p => p.Discount).ToList();
+                    break;
+                case "Размер скидки (по убыванию)":
+                    _sortedStats = _agentStats.OrderByDescending(p => p.Discount).ToList();
+                    break;
+                default:
+                    _sortedStats = _agentStats.ToList();
+                    break;
+            }
+
+            var selectedType = FilterByDateCb.SelectedItem as AgentType;
+            if (selectedType.ID != 0)
+                _sortedStats = _sortedStats.Where(p => p.TypeOfAgent == selectedType.Title).ToList();
+
+            _sortedStats = _sortedStats.Where(p => p.AgentName.ToLower().Contains(SearchTb.Text.ToLower())).ToList();
+
+            WorkloadLv.ItemsSource = _sortedStats.ToList();
+            WorkloadLv.Items.Refresh();
+
+        }
+
+        private class AgentStat
+        {
+            public int Id { get; set; }
+            public string AgentName { get; set; }
+            public int CountOfSales { get; set; }
+            public double Discount { get; set; }
+            public string Phone { get; set; }
+            public string TypeOfAgent { get; set; }
+
+            public string ImagesSource { get; set; }
+            public string DiscountColor { get; set; }
+
+            public AgentStat(string agentName, int sales, double discount,
+                string phone, string typeOfAgent, string source, int id)
+            {
+                AgentName = agentName;
+                CountOfSales = sales;
+                Discount = discount;
+                Phone = phone;
+                TypeOfAgent = typeOfAgent;
+                if (source == "/Images/")
+                    ImagesSource = "/Images/picture.png";
+                else
+                    ImagesSource = source;
+                Id = id;
+
+                if (discount >= 0.25)
+                    DiscountColor = "LightGreen";
+            }
+        }
+
+        private void SearchTb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateList();
+        }
+
+        private void FilterByDateCb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateList();
+        }
+
+        private void SortByDateCb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateList();
+        }
+
+        private void UpdateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var agent = (sender as Button).DataContext as AgentStat;
+
+            if(agent != null)
+            {
+                var agn = MainWindow.db.Agent.Where(a => a.ID == agent.Id).FirstOrDefault();
+                if(agn != null)
+                {
+                    UpdateAgentWindow updateAgent = new UpdateAgentWindow(agn);
+                    if(updateAgent.ShowDialog() == true)
+                    {
+                        UpdateList();
+                    }
+                }
+            }
+        }
+
+        private void AddAgentBtn_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateAgentWindow updateAgent = new UpdateAgentWindow();
+            if(updateAgent.ShowDialog() == true)
+            {
+                UpdateList();
+            }
+        }
+    }
+}
---
+++ ProductStore/Pages/Windows/UpdateAgentWindow.xaml.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.Win32;
+using ProductStore.DBContext;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProductStore.Pages.Windows
+{
+    /// <summary>
+    /// Interaction logic for UpdateAgentWindow.xaml
+    /// </summary>
+    public partial class UpdateAgentWindow : Window
+    {
+        private readonly Agent _agent;
+
+        public bool IsClosed { get; set; } = false;
+
+        public UpdateAgentWindow()
+        {
+            InitializeComponent();
+
+            TypesCb.ItemsSource = MainWindow.db.AgentType.ToList();
+            TypesCb.DisplayMemberPath = "Title";
+
+            AddBtn.Visibility = Visibility.Visible;
+            DeleteBtn.Visibility = Visibility.Collapsed;
+            UpdateBtn.Visibility = Visibility.Collapsed;
+        }
+
+        public UpdateAgentWindow(Agent agent)
+        {
+            InitializeComponent();
+            _agent = MainWindow.db.Agent.Attach(agent);
+
+            TypesCb.ItemsSource = MainWindow.db.AgentType.ToList();
+            TypesCb.DisplayMemberPath = "Title";
+
+            TitleTb.Text = agent.Title;
+            TypesCb.SelectedItem = agent.AgentType;
+            AddressTb.Text = agent.Address;
+            InnTb.Text = agent.INN;
+            KppTb.Text = agent.KPP;
+            DirectorTb.Text = agent.DirectorName;
+            PhoneTb.Text = agent.Phone;
+            EmailTb.Text = agent.Email;
+
+            AddBtn.Visibility = Visibility.Collapsed;
+            DeleteBtn.Visibility = Visibility.Visible;
+            UpdateBtn.Visibility = Visibility.Visible;
+        }
+
+        private void ImageBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(TitleTb.Text == string.Empty || AddressTb.Text == string.Empty 
+                || InnTb.Text == string.Empty || KppTb.Text == string.Empty 
+                || DirectorTb.Text == string.Empty || PhoneTb.Text == string.Empty
+                || EmailTb.Text == string.Empty)
+            {
+                MessageBox.Show("Enter the data");
+                return;
+            }
+
+            var selectedType = TypesCb.SelectedItem as AgentType;
+            if(selectedType == null)
+            {
+                MessageBox.Show("Choose an agent type");
+                return;
+            }
+
+            Agent agent = new Agent();
+            agent.Title = TitleTb.Text;
+            agent.Address = AddressTb.Text;
+            agent.INN = InnTb.Text;
+            agent.KPP = KppTb.Text;
+            agent.DirectorName = DirectorTb.Text;
+            agent.Phone = PhoneTb.Text;
+            agent.Email = EmailTb.Text;
+            agent.AgentTypeID = selectedType.ID;
+            agent.Priority = 1;
+
+            try
+            {
+                MainWindow.db.Agent.Add(agent);
+                MainWindow.db.SaveChanges();
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+            DialogResult = true;
+        }
+
+        private void UpdateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (TitleTb.Text == string.Empty || AddressTb.Text == string.Empty
+                || InnTb.Text == string.Empty || KppTb.Text == string.Empty
+                || DirectorTb.Text == string.Empty || PhoneTb.Text == string.Empty
+                || EmailTb.Text == string.Empty)
+            {
+                MessageBox.Show("Enter the data");
+                return;
+            }
+
+            var selectedType = TypesCb.SelectedItem as AgentType;
+            if (selectedType == null)
+            {
+                MessageBox.Show("Choose an agent type");
+                return;
+            }
+
+            _agent.Title = TitleTb.Text;
+            _agent.Address = AddressTb.Text;
+            _agent.INN = InnTb.Text;
+            _agent.KPP = KppTb.Text;
+            _agent.DirectorName = DirectorTb.Text;
+            _agent.Phone = PhoneTb.Text;
+            _agent.Email = EmailTb.Text;
+            _agent.AgentTypeID = selectedType.ID;
+            _agent.Priority = 1;
+
+            try
+            {
+                MainWindow.db.SaveChanges();
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+            DialogResult = true;
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var result = MessageBox.Show($"Удалить?", "Удаление", MessageBoxButton.YesNo);
+
+                if(result == MessageBoxResult.Yes)
+                {
+                    MainWindow.db.Agent.Remove(_agent);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            DialogResult = true;
+        }
+    }
+}
